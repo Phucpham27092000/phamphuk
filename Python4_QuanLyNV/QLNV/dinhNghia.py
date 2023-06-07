8e8302fb4fe7c1762134e0b12aad8cfa6ca7fd08
Phucpham27092000 <93719046+Phucpham27092000@users.noreply.github.com>
2023-06-07 10:43:05 +0700

    QuanLyNhanVien(HoanChinh)

---
+++ Python4_QuanLyNV/QLNV/dinhNghia.py
@@ -0,0 +1,7 @@
+class CongTy:
+    def __init__(self, ten, tuoi, quequan, chucvu, chamcong):
+        self.ten = ten
+        self.tuoi = tuoi
+        self.quequan = quequan
+        self.chucvu = chucvu
+        self.chamcong = chamcong
