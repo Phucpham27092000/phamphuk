83c27bd8d4da19c4dacdd1f9c3bc99e06bdfc285
Phucpham27092000 <93719046+Phucpham27092000@users.noreply.github.com>
2023-07-13 14:48:02 +0700

    Add files via upload

---
+++ GUI_QLHV/QuanLyHocVien/DInhNghia.py
@@ -0,0 +1,9 @@
+import mysql.connector
+def getConnection():
+    connection = mysql.connector.connect(host = 'localhost', 
+                                         user = 'root', 
+                                         passwd = '27092000', 
+                                         database = 'Quanlyhocvien', 
+                                         auth_plugin='mysql_native_password' 
+                                         )
+    return connection
