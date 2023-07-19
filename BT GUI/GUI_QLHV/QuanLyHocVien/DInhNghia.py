98f3369bf83e7bb08fcea13d3f30b12e7b82e9d0
Phucpham27092000 <93719046+Phucpham27092000@users.noreply.github.com>
2023-07-19 20:57:38 +0700

    Delete BT GUI directory

--- BT GUI/GUI_QLHV/QuanLyHocVien/DInhNghia.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-def getConnection():
-    connection = mysql.connector.connect(host = 'localhost', 
-                                         user = 'root', 
-                                         passwd = '27092000', 
-                                         database = 'Quanlyhocvien', 
-                                         auth_plugin='mysql_native_password' 
-                                         )
-    return connection
